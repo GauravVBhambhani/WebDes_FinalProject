7a4ef6b6e7c92fe010e76f0c364aee7423b6cf90
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2022-12-05 19:01:35 -0500

    resolved conflicts -G

--- dear-diary/src/App.js
+++ dear-diary/src/App.js
@@ -1,27 +1,56 @@
-import './App.css';
-import React from 'react';
+// import './App.css';
+// import React from 'react';
 
 
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Signup from './pages/Signup.page';
+// import Signup from './pages/Signup.page';
+// import Signin from './pages/Signin.page';
+// import About from './pages/About';
+
+// function App() {
+//   return (
+//     <div>
+//       <Router>
+//         <Routes>
+//           <Route path='/' element={<Signup />}></Route>
+//           <Route path='/signin' element={<Signin />}></Route>
+//           <Route path='/about' element={<About />}></Route>
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+
+// }
+
+// export default App;
+
+import "./App.css";
+import React from "react";
+
+import Signup from "./pages/Signup.page";
 import Signin from './pages/Signin.page';
-import About from './pages/About';
+import Password from "./pages/account/Password";
+import Account from "./pages/account/Account";
+import Profile from "./pages/account/Profile";
+import Home from "./pages/Home";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Signup />}></Route>
-          <Route path='/signin' element={<Signin />}></Route>
-          <Route path='/about' element={<About />}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
-
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path='/signin' element={<Signin />}></Route>
+        <Route path="/account" element={<Account />} />
+        <Route path="/account/profile" element={<Profile />} />
+        <Route path="/account/password" element={<Password />} />
+      </Routes>
+    </>
+    );
 }
 
 export default App;
