108cb6ffdbd630c93dddf0f985ce591253bbe372
Dhruv Patel <patel.dhruvr@northeastern.edu>
2022-12-09 01:40:17 -0500

    merge conflicts solved

--- dear-diary/src/pages/Signup.page.js
+++ dear-diary/src/pages/Signup.page.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import Signup from '../components/signup/Signup';
+import Footer from '../components/footer/Footer';
 
 const SplitScreen = () => {
   return (
-    <React.Fragment>
-      <section>
-        <div className='w-screen h-screen grid grid-cols-12 text-white text-4xl'>
+    // <React.Fragment>
+      // <div>
+        <div className='w-screen h-screen grid grid-rows-12 text-white text-4xl  md:grid-cols-12'>
 
           {/* Left section */}
-          <div className='w-full h-full centered col-span-4'>
-            <img src={require('../components/signup/assets/signup_landing.png')} alt='Landing page' className='text-black h-full w-full '/>
+          <div className='w-full h-full centered row-span-4 md:col-span-4'>
+            <img src={require('../components/assets/signup_landing.png')} alt='Landing page' className='text-black h-full w-full '/>
           </div>
 
           {/* Right section */}
-          <div className='w-full h-full text-black centered col-span-8'>
+          <div className='w-full h-full text-black centered row-span-4 md:col-span-8 pt-24'>
             <Signup />
           </div>
 
+          <footer className='w-screen h-screen grid grid-rows-12 text-xs'><Footer/></footer>
         </div>
-      </section>
-    </React.Fragment>
+        
+      // </div>
+      
+    // </React.Fragment>
+
+    
   );
 }
 
