d619d60daccf0e93559f06fa32100ab61247c23e
Dhruv Patel <patel.dhruvr@northeastern.edu>
2022-12-10 13:43:00 -0500

    merged solved

--- dear-diary/src/App.js
+++ dear-diary/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import React from 'react';
 import Signup from './pages/Signup.page';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Signin from './pages/Signin.page';
 import {
   // BrowserRouter as Router,
@@ -15,16 +19,14 @@ import TextEditor from '../src/components/Document/TextEditor';
 import Home from './pages/Home';
 import About from './pages/About';
 
+
 function App() {
   // const current = new Date();
   // const currentDate = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-
   return (
     <Routes>
-      <Route path="/" element={<Signup />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/dashboard" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/account" element={<Account />} />
       <Route path="/account/profile" element={<Profile />} />
       <Route path="/account/password" element={<Password />} />
